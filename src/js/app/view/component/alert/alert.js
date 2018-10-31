fb93a792416a654a4f7d0f1b056b53b31fe3da74
avadhesh6792 <avadheshbhatt92@gmail.com>
2018-10-31 13:37:53 +0530

    change in alert file

--- src/js/app/view/component/alert/alert.js
+++ src/js/app/view/component/alert/alert.js
@@ -1,1 +1,16 @@
-
+define(['text!app/view/component/alert/alert.html'],
+ function(alertHtml){
+    let alertView = Backbone.View.extend({
+        initialize: function(attrs){
+            this.type = attrs.type;
+            this.message = attrs.message;
+        },
+        template: _.template(alertHtml),
+        el: '#alert',
+        render: function(){
+            this.$el.html(this.template({message: this.message, type: this.type}));
+            return this;
+        }
+    });
+    return alertView;
+});
